d60b2fdf58d92b963ccbb3f01cdf8d51c5c980c7
C <earthhominid@gmail.com>
2023-04-12 17:13:35 -0300

    removed redundant files

--- components/guides/inventory/module-two/ModuleTwo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const ModuleTwo = (props: Props) => {
-  return <div>ModuleTwo</div>;
-};
-
-export default ModuleTwo;
--- components/guides/merchandise/Merchandise.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const Merchandise = (props: Props) => {
-  return <div>Merchandise</div>;
-};
-
-export default Merchandise;
